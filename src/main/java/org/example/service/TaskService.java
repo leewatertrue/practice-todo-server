fc88ffc5b49ad3c500769c269e6dd3d399c61891
leewatertrue <leewatertrue@naver.com>
2025-06-05 23:09:29 +0900

    ✨ Feat: API 기능 구현

--- src/main/java/org/example/service/TaskService.java
+++ src/main/java/org/example/service/TaskService.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.example.constants.TaskStatus;
 import org.example.model.Task;
 import org.example.model.TaskEntity;
@@ -12,6 +13,7 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -53,6 +55,37 @@ public class TaskService {
     public Task getOne(Long id) {
         var entitiy = this.getById(id);
         return this.entitiyToObject(entitiy);
+    }
+
+    public Task update(Long id, String title, String description, LocalDate dueDate) {
+        var exist= this.getById(id);
+        exist.setTitle(Strings.isEmpty(title) ?
+                exist.getTitle() : title);
+        exist.setDescription(Strings.isEmpty(description) ?
+                exist.getDescription():description);
+        exist.setDueDate(Objects.isNull(dueDate) ?
+                exist.getDueDate():Date.valueOf(dueDate));
+
+        var updated=this.taskRepository.save(exist);
+        return this.entitiyToObject(updated);
+    }
+
+    public Task updateStatus(Long id, TaskStatus status) {
+        var entity = this.getById(id);
+        entity.setStatus(status);
+        var saves=this.taskRepository.save(entity);
+
+        return this.entitiyToObject(saves);
+    }
+
+    public boolean delete(Long id) {
+        try{
+            this.taskRepository.deleteById(id);
+        } catch(Exception e) {
+            log.error("an error occured while deleting task", e.toString());
+            return false;
+        }
+        return true;
     }
 
     private TaskEntity getById(Long id) {
--- src/main/java/org/example/web/TaskController.java
+++ src/main/java/org/example/web/TaskController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.constants.TaskStatus;
 import org.example.service.TaskService;
+import org.example.web.vo.ResultResponse;
 import org.example.web.vo.TaskRequest;
+import org.example.web.vo.TaskStatusRequest;
 import org.springframework.http.ResponseEntity;
 import org.example.model.Task;
 import org.springframework.stereotype.Controller;
@@ -68,5 +70,51 @@ public class TaskController {
     public ResponseEntity<List<Task>> getByStatus(@PathVariable TaskStatus status) {
         var result=this.taskService.getByStatus(status);
         return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 특정 ID에 해당하는 할일을 수정
+     * @param id 할일 ID
+     * @param req 수정할 할일 정보
+     * @return 수정된 할일 객체
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id,
+                                           @RequestBody TaskRequest req) {
+        var result=this.taskService.update(id,
+                req.getTitle(),
+                req.getDescription(),
+                req.getDueDate());
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 특정 ID에 해당하는 할일의 상태를 수정
+     * @param id 할일 ID
+     * @param req 수정할 할일 상태 정보
+     * @return 수정된 할일 객체
+     */
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Task> updateTaskStatus(@PathVariable Long id,
+                                                 @RequestBody TaskStatusRequest req) {
+        var result=this.taskService.updateStatus(id, req.getStatus());
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 특정 ID에 해당하는 할일을 삭제
+     * @param id 삭제할 할일 ID
+     * @return 삭제 결과를 담은 응답 객체
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResultResponse> deleteTask(@PathVariable Long id) {
+        var result=this.taskService.delete(id);
+        return ResponseEntity.ok(new ResultResponse(result));
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<TaskStatus[]> getAllStatus() {
+        var status=TaskStatus.values();
+        return ResponseEntity.ok(status);
     }
 }
